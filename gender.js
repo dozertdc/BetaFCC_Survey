1fdec91c78555cb456bdca966571b23c0d06f73b
Travis Cooper <dozertdc@gmail.com>
2017-06-30 00:57:36 +0000

    Add weight class differences for Male and Female

--- gender.js
+++ gender.js
@@ -1,3 +1,16 @@
+
+if(document.getElementById('male').checked) {
+  //Male radio button is checked
+    document.getElementById("WomensClasses").style.display = "none";
+    document.getElementById("MensClasses").style.display = "block";
+}  
+    
 document.getElementById("male").addEventListener("click", function(){
     document.getElementById("WomensClasses").style.display = "none";
+    document.getElementById("MensClasses").style.display = "block";
+});
+
+document.getElementById("female").addEventListener("click", function(){
+    document.getElementById("MensClasses").style.display = "none";
+    document.getElementById("WomensClasses").style.display = "block";
 });
